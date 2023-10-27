ddc431ad6ff0d61470ebe97823a743ace0a698db
Júlia Sendra <juliasendra98@gmail.com>
2023-10-27 09:33:56 +0200

    step 2 and 3

--- js/main.js
+++ js/main.js
@@ -1,33 +1,99 @@
+
 class Player {
     constructor() {
-        this.positionX = 50;
-        this.positionY = 0;
+        // initialize properties
         this.height = 10;
         this.width = 20;
-
-        const playerElm = document.getElementById("player");
-        playerElm.style.width = this.width + "vw";
-        playerElm.style.height = this.height + "vh";
-
-        playerElm.style.left = this.positionX + "vw";
-        playerElm.style.bottom = this.positionY + "vh";
+        this.positionX = 50 - (this.width / 2);
+        this.positionY = 0;
         
+
+        // dom manipulation to reflect initial values (size, position)
+        this.playerElm = document.getElementById("player");
+        this.playerElm.style.width = this.width + "vw";
+        this.playerElm.style.height = this.height + "vh";
+        this.playerElm.style.left = this.positionX + "vw";
+        this.playerElm.style.bottom = this.positionY + "vh";
     }
-    moveLeft(){
+    moveLeft() {
         this.positionX--;
-        const elm = document.getElementById("player");
-        elm.style.left = this.positionX + "vw";
+        this.playerElm.style.left = this.positionX + "vw";
     }
-    moveRight(){
+    moveRight() {
         this.positionX++;
-        const elm = document.getElementById("player");
-        elm.style.left = this.positionX + "vw"
-
+        this.playerElm.style.left = this.positionX + "vw";
     }
-
 }
 
+
+class Obstacle {
+    constructor(){
+        this.height = 10;
+        this.width = 30;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+        this.positionY = 100;
+        this.obstacleElm = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // step1: create the element
+        this.obstacleElm = document.createElement("div");
+
+        // step2: add content or modify
+        this.obstacleElm.classList.add("obstacle");
+        this.obstacleElm.style.width = this.width + "vw";
+        this.obstacleElm.style.height = this.height + "vh";
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+        
+        // step3: append to the dom: `parentElm.appendChild()`
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.obstacleElm);
+    }
+    moveDown(){
+        this.positionY--;
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+    }
+}
+
+
+
+
 const player = new Player();
+
+const obstaclesArr = []; // will store instances of the class Obstacle
+
+
+// create obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+}, 3000);
+
+
+
+// update obstacles
+setInterval(() => {
+    obstaclesArr.forEach( (obstacleInstance) => {
+        // move
+        obstacleInstance.moveDown();
+
+        // detect collision
+        if (
+            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            player.positionX + player.width > obstacleInstance.positionX &&
+            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            player.positionY + player.height > obstacleInstance.positionY
+        ) {
+            // Collision detected!
+            // console.log("game over!");
+            location.href = "./gameover.html";
+        }
+    });
+}, 30);
+
+
 
 document.addEventListener("keydown", (e) => {
     switch (e.code) {
